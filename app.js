07b648eed3fa798fdee9fdf9986a658944f44852
Brian Smith <bjtsmith23@gmail.com>
2025-06-13 11:48:56 -0400

    secondary

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const button2 = document.getElementById('button2');
 const maxPlayerCount = 8;
 let playerCount;
 let playerDropdown = document.createElement('select');
+let numberDropdown = document.createElement('select');
+
 const firstEl = document.getElementById('first');
 const secondEl = document.getElementById('second');
 const thirdEl = document.getElementById('third');
@@ -27,6 +29,19 @@ function start() {
     playerCount = playerDropdown.value;
     firstEl.classList.add('hide');
     secondEl.classList.toggle('hide');
+    for (let i = 0; i < playerCount; i++) {
+        const divEl = document.createElement('div');
+        const inputEl = document.createElement('input');
+        for (let i = 0; i <= 20; i++){
+            const option = document.createElement('option');
+            option.value = i;
+            option.textContent = i;
+            numberDropdown.appendChild(option);
+        }
+        divEl.appendChild(inputEl);
+        divEl.appendChild(numberDropdown);
+        secondEl.appendChild(divEl);
+    }
 }
 
 function startGame() {
