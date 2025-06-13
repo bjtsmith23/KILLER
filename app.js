6f07fbeb480bcccb16623f9f744ef02d08078213
Brian Smith <bjtsmith23@gmail.com>
2025-06-13 11:07:28 -0400

    adding fundamental code...

---
+++ app.js
@@ -0,0 +1,39 @@
+const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+
+const maxPlayerCount = 8;
+let playerCount;
+let playerDropdown = document.createElement('select');
+const firstEl = document.getElementById('first');
+const secondEl = document.getElementById('second');
+const thirdEl = document.getElementById('third');
+
+
+
+for (let i = 0; i <= maxPlayerCount; i++){
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    playerDropdown.appendChild(option);
+}
+
+firstEl.appendChild(playerDropdown);
+
+const menu = document.getElementById('menu');
+const playerVal = menu;
+
+
+function start() {
+    playerCount = playerDropdown.value;
+    firstEl.classList.add('hide');
+    secondEl.classList.toggle('hide');
+}
+
+function startGame() {
+    secondEl.classList.toggle('hide');
+    thirdEl.classList.toggle('hide');
+
+}
+
+button.addEventListener('click', start);
+button2.addEventListener('click', startGame);
